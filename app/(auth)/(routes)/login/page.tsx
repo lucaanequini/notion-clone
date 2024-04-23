bd8d379225c2ca3b8a509753f09470f39546b437
Luca Anequini Antoniazzi <lucanequini@gmail.com>
2024-04-22 22:16:45 -0300

    landing page created

--- app/(auth)/(routes)/login/page.tsx
+++
@@ -1,6 +0,0 @@
-const LoginPage = () => {
-    return (
-        <p>login</p>
-    )
-}
-export default LoginPage
--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,6 +0,0 @@
-const RegisterPage = () => {
-    return (
-        <p>register</p>
-    )
-}
-export default RegisterPage
