7d83ce39a7377a7a564b7b28dc14338a6772ffd1
Luca Anequini Antoniazzi <lucanequini@gmail.com>
2024-04-23 22:48:53 -0300

    created the note schema and first functions, and tested sidebar item with the note's title

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,10 +5,14 @@ import { useMediaQuery } from "usehooks-ts"
 import { UserItem } from "./user-item"
 
 import { cn } from "@/lib/utils"
+import { useQuery } from "convex/react"
+import { api } from "@/convex/_generated/api"
 
 export const Navigation = () => {
     const pathName = usePathname()
     const isMobile = useMediaQuery('(max-width: 768px')
+
+    const documents = useQuery(api.documents.get)
 
     const isResizingRef = useRef(false)
     const sidebarRef = useRef<ElementRef<'aside'>>(null)
@@ -111,7 +115,9 @@ export const Navigation = () => {
                     <UserItem />
                 </div>
                 <div className="mt-4">
-                    <p>Documents</p>
+                    {documents?.map((document) => (
+                        <p key={document._id}>{document.title}</p>
+                    ))}
                 </div>
                 <div
                     onMouseDown={handleMouseDown}
