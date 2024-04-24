ee9d4e3a55ae268a018da8b0b9c86daf86a89036
Luca Anequini Antoniazzi <lucanequini@gmail.com>
2024-04-23 22:23:19 -0300

    created a first content on documents page and created also a user settings display

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -2,6 +2,7 @@ import { ChevronsLeft, MenuIcon } from "lucide-react"
 import { usePathname } from "next/navigation"
 import { ElementRef, useRef, useState, useEffect } from "react"
 import { useMediaQuery } from "usehooks-ts"
+import { UserItem } from "./user-item"
 
 import { cn } from "@/lib/utils"
 
@@ -107,7 +108,7 @@ export const Navigation = () => {
                     <ChevronsLeft className="h-6 w-6" />
                 </div>
                 <div>
-                    <p>Action Items</p>
+                    <UserItem />
                 </div>
                 <div className="mt-4">
                     <p>Documents</p>
